10ddab830b880ddc435f5095c32fde0240858235
Petr Efimov (Shkoda) <mail@efimov-it.info>
2022-08-05 16:40:47 +0300

    Small integration fix

--- src/views/MainPage/index.jsx
+++ src/views/MainPage/index.jsx
@@ -1,10 +1,13 @@
 import { Component } from "react";
+import ContactsBlock from "../../components/contactsBlock";
 import TopBanner from "../../components/TopBanner";
 
 export default class MainPage extends Component {
     render() {
         return <main>
             <TopBanner/>
+
+            <ContactsBlock />
         </main>
     }
 }
