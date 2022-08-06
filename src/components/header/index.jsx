310ccaeefd4f0881b387a706fe77112ae7242cee
efimov-it <apple-i-shop@yande.ru>
2022-08-07 00:11:47 +0300

    Add navigation

---
+++ src/components/header/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+import './style.scss';
+import LogoImg from '../../assets/img/logo.png';
+
+class Header extends React.Component {
+    render () {
+        return (
+            <header className='header'>
+                <Link to="/" className='header_logo'>
+                    <img src={LogoImg} alt='' className='header_logoImg' />
+                </Link>
+                <nav className='header_nav'>
+                    <ul className='header_navList'>
+                        <li className='header_navItem'>
+                            <Link className='header_navLink' to="/where-to-buy">Где купить</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        );
+    }
+}
+
+export default Header;
