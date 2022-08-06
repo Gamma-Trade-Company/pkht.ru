d598328e3eda9497f4eb48e2314bae30bd35c6e9
efimov-it <apple-i-shop@yande.ru>
2022-08-07 01:27:21 +0300

    Add prerendering and open graph markup to inde.html

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot, hydrateRoot} from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const rootElement = document.getElementById("root");
+const cRoot = createRoot(rootElement);
+const hRoot = hydrateRoot(rootElement);
+
+const reactApp = () => (
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+
+if (rootElement.hasChildNodes()) {
+  hRoot.render(reactApp());
+} else {
+  cRoot.render(reactApp());
+}
